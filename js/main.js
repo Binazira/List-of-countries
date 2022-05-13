fe56f6f081846a8d40b2eec6d2da8a234de79fdf
Binazir <binazir@MacBook-Air-Binazir.local>
2022-05-13 16:17:46 +0600

    change position

--- js/main.js
+++ js/main.js
@@ -6,15 +6,6 @@
 // API: https://restcountries.eu/rest/v2/all
 
 let tbody = document.getElementById("tbody");
-let code = document.getElementById("code");
-let name = document.getElementById("name");
-let flag = document.getElementById("flag");
-let population = document.getElementById("population");
-let capital = document.getElementById("capital");
-let continents = document.getElementById("continents");
-let td = document.createElement("td");
-td.id = "td";
-
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
